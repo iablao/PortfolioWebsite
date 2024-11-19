839bf292a622037503c8ca89873b3a635574d5fe
iablao <iablao@sdsu.edu>
2024-11-18 17:22:25 -0800

    install pydantic + update config.py

--- portfoliowebsite/src/portfoliowebsite/core/config.py
+++ portfoliowebsite/src/portfoliowebsite/core/config.py
@@ -1,1 +1,15 @@
-password = 123
+from dotenv import load_dotenv
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import FilePath
+
+load_dotenv()
+
+class Settings(BaseSettings): 
+
+    SQLITE_FILE_NAME: FilePath
+
+    model_config: SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+
+@lru_cache
+def get_settings() -> Settings: 
+    return Settings()
