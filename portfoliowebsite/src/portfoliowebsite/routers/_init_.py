94ef207fdcae715c82eea4c1cbb3a38213299c7d
iablao <iablao@sdsu.edu>
2024-11-22 14:28:12 -0800

    added _init_.py file to router

---
+++ portfoliowebsite/src/portfoliowebsite/routers/_init_.py
@@ -0,0 +1,11 @@
+from fastapi import APIRouter
+
+from portfoliowebsite.router import category, user, transaction
+
+api_router = APIRouter()
+
+api_router.include_router(user.router, prefix="/users", tags=["Users"])
+api_router.include_router(category.router, prefix="/categories", tags=["Categories"])
+api_router.include_router(
+    transaction.router, prefix="/transactions", tags=["Transactions"]
+)
