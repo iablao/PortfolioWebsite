c5871ea9c86615de11b068f0a7f4e2200df2bb06
Ester <enabaty5582@sdsu.edu>
2024-11-26 10:34:47 -0800

    project tree is update

--- portfoliowebsite/src/portfoliowebsite/schemas.py
+++ portfoliowebsite/src/portfoliowebsite/schemas.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from pydantic import BaseModel
 
 class UserBase(BaseModel):
@@ -9,6 +10,6 @@ class UserCreate(UserBase):
 
 class User(UserBase):
     id: int
-
+    created_at: datetime 
     class Config:
         orm_mode = True
