9c200c4d035ef0179d84a63c6c193cc63e351419
iablao <iablao@sdsu.edu>
2024-11-26 11:59:10 -0800

    update main.py

--- portfoliowebsite/src/portfoliowebsite/main.py
+++ portfoliowebsite/src/portfoliowebsite/main.py
@@ -1,13 +1,16 @@
 from fastapi import FastAPI
 from portfoliowebsite.core.database import Base, engine
+from portfoliowebsite.routers import auth, portfolio 
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+app.include_router(auth.router)
+app.include_router(portfolio.router)
 
-# app.include_router(api_router)
 
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
