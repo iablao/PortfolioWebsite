87e78567cde400da9b0e46e07ec3aa9d015ce89d
iablao <iablao@sdsu.edu>
2024-11-30 23:45:54 -0800

    added UserCreate and UserLogin class in user.py

--- portfoliowebsite/src/portfoliowebsite/schemas/user.py
+++ portfoliowebsite/src/portfoliowebsite/schemas/user.py
@@ -2,16 +2,24 @@ from datetime import datetime
 from pydantic import BaseModel, EmailStr
 from typing import Optional
 
+# Base model shared across other user models
 class UserBase(BaseModel):
     username: str
     email: EmailStr
     role: Optional[str] = "user"  # optional role, defaults to "user"
 
-class UserCreate(BaseModel):
+# Model for creating a user
+class UserCreate(UserBase):
     password: str
     is_verified: Optional[bool] = None
     is_active: Optional[bool] = None
 
+# Model for user login
+class UserLogin(BaseModel):
+    email: EmailStr
+    password: str
+
+# Model representing a user in the database
 class User(BaseModel):
     id: int
     is_active: bool
@@ -22,13 +30,12 @@ class User(BaseModel):
     class Config:
         from_attributes = True
 
-class UserResponse(BaseModel):
+# Model for user responses
+class UserResponse(UserBase):
     id: int
-    username: str
-    email: EmailStr
-    role: Optional[str]  # Return the role in the response model if needed
     created_at: datetime
     updated_at: Optional[datetime] = None  # Include updated_at if needed
 
     class Config:
         from_attributes = True
+
