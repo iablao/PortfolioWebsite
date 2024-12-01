123a2c1d729537fbfa2eefbf0a367612be62e92f
iablao <iablao@sdsu.edu>
2024-11-30 22:36:44 -0800

    updated header file (components)

---
+++ frontend/portfoliowebsite/src/app/components/Header.js
@@ -0,0 +1,11 @@
+const Header = () => (
+    <header>
+    <nav>
+    <a href="/">Home</a>
+    <a href="/about">About</a>
+    <a href="/projects">Projects</a>
+    <a href="/contact">Contact</a>
+    </nav>
+    </header>
+    );
+    export default Header;
