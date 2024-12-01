48aadbb40fc0b5e49d974c9c0e281c946d920fd2
Ester <enabaty5582@sdsu.edu>
2024-11-30 22:59:07 -0800

    page folder is done

---
+++ frontend/portfoliowebsite/src/app/pages/projects.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+const Projects = () => {
+const [projects, setProjects] = useState([]);
+useEffect(() => {
+axios
+.get(`${process.env.NEXT_PUBLIC_API_URL}/projects`)
+.then((response) => setProjects(response.data))
+.catch((error) => console.error(error));
+}, []);
+return (
+<div>
+<h1>My Projects</h1>
+<ul>
+{projects.map((project) => (
+<li key={project.id}>{project.name}</li>
+))}
+</ul>
+</div>
+);
+};
+export default Projects;
